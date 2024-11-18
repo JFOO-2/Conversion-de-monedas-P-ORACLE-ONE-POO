c5439cc73799f89972924a4859b802c57face5a0
Jesus <jesusfi258@hotmail.com>
2024-11-17 22:07:07 -0600

    Clase record para no realizar getters ni setters ni toString ni su similar

---
+++ P-ONE-PROYECTO-CONVERSOR-DE-MONEDAS-POO/DatosRequeridosApi.java
@@ -0,0 +1,3 @@
+public record DatosRequeridosApi(String base_code, String target_code, double conversion_rate, double conversion_result) {
+// Esta clase record ya contiene todos los getters y setters, to string y demas metodos que se necesitan realizar simplificando el codigo
+}
