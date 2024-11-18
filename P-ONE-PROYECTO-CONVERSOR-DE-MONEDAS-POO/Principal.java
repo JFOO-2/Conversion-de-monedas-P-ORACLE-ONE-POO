bad763e751ede06aac9aeb3c2329c1bc79eaffd3
Jesus <jesusfi258@hotmail.com>
2024-11-17 22:18:38 -0600

    Se instancia clase que tiene el constructor que toma de referencia el record, se manda a llamar al constructor creado desde ImportAPI, el cual sirve para Llamar a los datos de DatosRequeridos, clase record, proporcionando los datos que pide el constructor para el link de la api

--- P-ONE-PROYECTO-CONVERSOR-DE-MONEDAS-POO/Principal.java
+++ P-ONE-PROYECTO-CONVERSOR-DE-MONEDAS-POO/Principal.java
@@ -1,30 +1,13 @@
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class Principal {
-
     public static void main(String[] args) throws IOException, InterruptedException {
-        
-        String urlApi = "https://v6.exchangerate-api.com/v6/92e3a1c050624b43759343a0/pair/USD/MXN";
 
-        // Class HttpRequest
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-              .uri(URI.create(urlApi))
-              .build();
+        Scanner t = new Scanner(System.in);
 
-              // Interface HttpResponse<T>
-
-              HttpResponse<String> response = client
-              .send(request, BodyHandlers.ofString()); 
-
-
-              String json = response.body();
-              System.out.println(json);
-              
+        ImportAPI datos = new ImportAPI();
+        DatosRequeridosApi datosr = datos.conversor(t.next(),t.next(),t.nextDouble());
+        System.out.println(datosr);
     }
 }
