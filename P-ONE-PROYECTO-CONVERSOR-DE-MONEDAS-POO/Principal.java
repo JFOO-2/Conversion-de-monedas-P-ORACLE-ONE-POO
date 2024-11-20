37ec597c81de684cb6aef972a3fb7657928dd630
Jesus <jesusfi258@hotmail.com>
2024-11-20 17:50:26 -0600

    Ciclo while para peticiones de usuario

--- P-ONE-PROYECTO-CONVERSOR-DE-MONEDAS-POO/Principal.java
+++ P-ONE-PROYECTO-CONVERSOR-DE-MONEDAS-POO/Principal.java
@@ -1,13 +1,16 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        // Instanceo la Api
+        var t = new Scanner(System.in);
+        String opcion = "";
+        while (!opcion.equalsIgnoreCase("no")) {
+            // Instanceo la Api
             ImportAPI datosDeApi = new ImportAPI();
-        // Instanceo la entrada de datos del usuario
+            // Instanceo la entrada de datos del usuario
             DatosInUsuario datosDeUsuario = new DatosInUsuario();
-        // Llamo al metodo datosentradausuario que contiene los valores de los setters
+            // Llamo al metodo datosentradausuario que contiene los valores de los setters
             datosDeUsuario.datosEntradaUsuario();
         /*Lanzo record, Toma el valor de la instacia la clase ImportAPI (que llama
         a el Constructor de record implementando al metodo conversor
@@ -18,7 +21,12 @@ public class Principal {
                     datosDeUsuario.getValorAConvertir()
             );
 
-        System.out.println(datosParaIniciarConversorApi);
+            System.out.println(datosParaIniciarConversorApi);
+
+            System.out.println("Quiere hacer otra conversion: ");
+            opcion = t.next();
+        }
+        System.out.println("Proceso finalizado, gracias por usar nuetro servicio");
 
     }
 }
