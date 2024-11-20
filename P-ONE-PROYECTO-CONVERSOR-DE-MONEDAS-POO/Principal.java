85cb24019f1c7de2b0d73d3e8e2a91eebd61a418
Jesus <jesusfi258@hotmail.com>
2024-11-20 16:57:55 -0600

    Clase Main

--- P-ONE-PROYECTO-CONVERSOR-DE-MONEDAS-POO/Principal.java
+++ P-ONE-PROYECTO-CONVERSOR-DE-MONEDAS-POO/Principal.java
@@ -1,13 +1,24 @@
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        Scanner t = new Scanner(System.in);
+        // Instanceo la Api
+            ImportAPI datosDeApi = new ImportAPI();
+        // Instanceo la entrada de datos del usuario
+            DatosInUsuario datosDeUsuario = new DatosInUsuario();
+        // Llamo al metodo datosentradausuario que contiene los valores de los setters
+            datosDeUsuario.datosEntradaUsuario();
+        /*Lanzo record, Toma el valor de la instacia la clase ImportAPI (que llama
+        a el Constructor de record implementando al metodo conversor
+         */
+            DatosRequeridosApi datosParaIniciarConversorApi = datosDeApi.conversor(
+                    datosDeUsuario.getDivisa(),
+                    datosDeUsuario.getDivisaAConvertir(),
+                    datosDeUsuario.getValorAConvertir()
+            );
 
-        ImportAPI datos = new ImportAPI();
-        DatosRequeridosApi datosr = datos.conversor(t.next(),t.next(),t.nextDouble());
-        System.out.println(datosr);
+        System.out.println(datosParaIniciarConversorApi);
+
     }
 }
